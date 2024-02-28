68e7c54cda730961fca9136d5739e719e70c38fd
RCOHEN209 <RCOHEN209@bloomberg.net>
2024-02-28 12:05:36 -0500

    Edit

--- calculator.js
+++ calculator.js
@@ -1,3 +1,18 @@
 function add(a, b) {
-    return a + b
+    return a + b;
 }
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiple(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a * b;
+}
+
+let num1;
+let operator = ['+', ]
